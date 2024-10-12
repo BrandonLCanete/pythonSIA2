a7ae6684752f6563e6308a50ea95494043aa07ef
Brandon L. Canete <brandoncanete35@gmail.com>
2024-10-12 12:38:11 +0800

    Python SIA 2

---
+++ basic.py
@@ -0,0 +1,12 @@
+# Variables
+one = 10
+two = 20
+
+# print Try Basic Commands on Your Own Machine
+print("Try Basic Commands on Your Own Machine")
+
+# add 3 + 3
+print( 3 + 3)
+
+# add one and two variable
+print(one + two)
---
+++ controlstructure.py
@@ -0,0 +1,11 @@
+# Variables
+name = "Brandon"
+age = 21
+
+if name == "Nice":
+    print("Hello " +  name) 
+elif name == "Brandon" and age == 21:
+    print("Wow Hello " + name)
+else:
+    print("No match")
+
